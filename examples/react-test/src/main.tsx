19b2dc4756391ab4444212c713856b88ac1681ac
jsricardo <jsricardo@qq.com>
2025-08-31 16:23:44 +0800

    feat: react适配器测试

---
+++ examples/react-test/src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Monitor, ReactAdapter } from '@rc-monitor/rc-monitor';
+import {
+  BrowserErrorPlugin,
+  BrowserBehaviorPlugin,
+  BrowserPerformancePlugin,
+} from '@rc-monitor/plugins';
+
+const monitor = Monitor.getMonitor({
+  appId: '123',
+  endpoint: 'http://localhost:3000/api/error',
+  reporterType: 'xhr',
+});
+
+monitor.use(new BrowserErrorPlugin());
+monitor.use(new BrowserBehaviorPlugin());
+monitor.use(new BrowserPerformancePlugin());
+
+const { ErrorBoundary } = ReactAdapter(monitor);
+
+createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ErrorBoundary>
+);
